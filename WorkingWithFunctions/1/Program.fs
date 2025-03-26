eb77a4d11c91727394a7d742abf302584d25aeb1
llKristina <kristina20052020@gmail.com>
2025-03-26 20:59:58 +0300

    lab 5 task 3
    Реализация функции нахождения площади круга, реализация вычисления объема цилиндра и вывод результата

---
+++ WorkingWithFunctions/1/Program.fs
@@ -0,0 +1,28 @@
+﻿open System
+
+let pi = 3.14159
+
+let areaOfCircle r =
+    pi * r * r
+
+let volumeOfCylinder r h =
+    areaOfCircle r * h
+
+
+[<EntryPoint>]
+let main argv =
+
+    printfn "Введите радиус круга:"
+    let r = float(Console.ReadLine())
+
+    printfn "Введите высоту цилиндра:"
+    let h = float(Console.ReadLine())
+
+    let area = areaOfCircle r
+    let volume = volumeOfCylinder r h
+
+
+    printfn "Площадь круга: %.2f" area
+    printfn "Объем цилиндра: %.2f" volume
+
+    0 
