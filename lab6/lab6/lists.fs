07c15008d824aa5f74f312bee8c93d2de55c8470
llKristina <kristina20052020@gmail.com>
2025-04-08 21:40:36 +0300

    lab 6 task 6

---
+++ lab6/lab6/lists.fs
@@ -0,0 +1,31 @@
+﻿//задание 6
+type Tree =
+    | Node of string * Tree * Tree
+    | Empty
+
+let rec add value tree =
+    match tree with
+    | Empty -> Node(value,Empty,Empty)
+    | Node(data, left, right) when value < data -> Node(data, add value left, right)
+    | Node(data, left, right) when value > data -> Node(data, left, add value right)
+    |_ -> tree
+
+let rec search value tree =
+    match tree with
+    | Empty -> false
+    | Node(data, left, right) when value = data -> true 
+    | Node(data, left, right) when value < data -> search value left
+    | Node(data, left, right) when value > data -> search value right
+
+let myTree =
+    Empty
+    |> add "cat"
+    |> add "dog"
+    |> add "apple"
+    |> add "banana"
+
+let found = search "dog" myTree
+let notFound = search "elephant" myTree
+
+printfn "Found 'dog': %b" found
+printfn "Found 'elephant' %b" notFound
