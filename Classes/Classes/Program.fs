7fbbe004e6bcff71d5daf28876808a6051964ec8
llKristina <kristina20052020@gmail.com>
2025-04-16 21:20:13 +0300

    lab 5

--- Classes/Classes/Program.fs
+++ Classes/Classes/Program.fs
@@ -1,5 +1,5 @@
 ﻿open System
-
+//lab 4
 type IPrint =
     abstract member Print : unit -> unit
 
@@ -75,3 +75,52 @@ figures
     | Square(s) -> $"Квадрат {s}x{s}: площадь = {area:.2f}"
     | Circle(r) -> $"Круг r={r}: площадь = {area:.2f}")
 |> List.iter (printfn "%s")
+
+
+//lab 5
+module ListMonad =
+    let map f list = List.map f list
+
+    let apply fs xs =
+        [ for f in fs do
+          for x in xs do
+              yield f x ]
+
+    let pure' x = [x]
+
+    let bind f list = List.collect f list
+    let return' x = [x]
+
+    module Laws =
+        let Identity list =
+            map id list = list
+
+        let Composition f g list =
+            map (f >> g) list = (map f >> map g) list
+
+        let licativeIdentity list =
+            apply (pure' id) list = list
+
+        let ApplicativeHomomorphism f x =
+            apply (pure' f) (pure' x) = pure' (f x)
+
+        let ApplicativeInterchange f x =
+            apply f (pure' x) = apply (pure' (fun g -> g x)) f
+
+        let MonadLeftIdentity f x =
+            bind f (return' x) = f x
+
+        let MonadRightIdentity list =
+            bind return' list = list
+
+let list = [10; 2; 13; 6; 15]
+let double x = x * 2
+let multiplyAndAdd x = [x; x * 10]
+
+printfn "Функтор map (удвоение): %A" (ListMonad.map double list)
+printfn "Аппликативный apply (декартово произведение): %A" (ListMonad.apply [double; (fun x -> x + 100)] list)
+printfn "Монада bind (размножение элементов): %A" (ListMonad.bind multiplyAndAdd list)
+printfn "Функтор - закон идентичности: %b" (ListMonad.Laws.Identity list)
+printfn "Функтор - закон композиции: %b" (ListMonad.Laws.Composition((+)1) double list)
+printfn "Аппликативный - закон гомоморфизма: %b" (ListMonad.Laws.ApplicativeHomomorphism double 5)
+printfn "Монада - левосторонняя идентичность: %b" (ListMonad.Laws.MonadLeftIdentity multiplyAndAdd 5)
