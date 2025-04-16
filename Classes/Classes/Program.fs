df6bfe7265fe5349888902d4419b8477c24a3c8c
llKristina <kristina20052020@gmail.com>
2025-04-16 20:31:18 +0300

    lab 4 task 1-3

---
+++ Classes/Classes/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+[<AbstractClass>]
+type GeometricFigure() =
+    abstract member Area : float
+
+type Rectangle(width: float, height: float) =
+    inherit GeometricFigure()
+    member this.Width = width
+    member this.Height = height
+    override this.Area = 
+        this.Width * this.Height
